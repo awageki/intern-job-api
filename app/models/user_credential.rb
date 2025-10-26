c37696c94f6e712681f8e5e9e7765b5f5f481359
awageki <ikeo.hogehoge@gmail.com>
2025-10-26 14:39:43 +0000

    feat: UserCredentialモデルにバリデーションを追加

--- app/models/user_credential.rb
+++ app/models/user_credential.rb
@@ -1,3 +1,6 @@
 class UserCredential < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :email, presence: true, format: URI::MailTo::EMAIL_REGEXP, uniqueness: { case_sensitive: false }
 end
